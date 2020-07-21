156edd412000a87d2b1903a0f06ebd3b8940842b
Steve Holland <sdh4@iastate.edu>
2020-07-21 18:59:34 -0500

    Add old VibroSim_build_release script

---
+++ VibroSim_build_release.sh
@@ -0,0 +1,57 @@
+# !/bin/bash
+# Usage VibroSim_build_release.sh <VERSION>
+#
+# where <VERSION> is e.g. 1.0.0 and matches VibroSim-<VERSION> tags in each repo. 
+#export DATE=`date -I`
+
+export VERSION=$1
+
+export VIBROSIM_REPOS="angled_friction_model closure_measurements crackclosuresim2 crackheat_surrogate2 tortuosity_tracing vibro_estparam VibroSim_COMSOL VibroSim_Simulator VibroSim_WelderModel crack_heatflow limatix heatsim2"
+
+if test `hostname` != ahab.cnde.iastate.edu ; then 
+  echo 'WARNING: Building VibroSim release from incorrect host'
+  hostname
+fi
+
+
+
+for repo in $VIBROSIM_REPOS ; do 
+  cd /usr/local/src/$repo
+  git update-index --refresh
+  
+  if ! git diff-index --quiet HEAD -- ; then
+    echo >&2 "Error in repository $repo$: There are uncommited changes"
+  fi
+
+
+  if test $(git rev-parse master) != $(git rev-parse VibroSim-$VERSION^{commit}); then
+      echo "WARNING: Repository $repo: VibroSim-$VERSION does not match the current head of the master branch"
+  fi
+
+done
+
+mkdir /tmp/VibroSim-$VERSION
+cd /tmp/VibroSim-$VERSION
+
+echo $VIBROSIM_REPOS
+
+for repo in $VIBROSIM_REPOS ; do 
+  git clone ssh://ahab.cnde.iastate.edu//usr/local/src/$repo 
+done
+
+# handle limatix sub-repos
+git clone ssh://ahab.cnde.iastate.edu//usr/local/src/limatix/limatix/canonicalize_path limatix/limatix/canonicalize_path
+
+git clone ssh://ahab.cnde.iastate.edu//usr/local/src/limatix/limatix/dc_lxml_treesync limatix/limatix/dc_lxml_treesync
+
+for repo in $VIBROSIM_REPOS ; do 
+  cd /tmp/VibroSim-$VERSION/$repo
+  git checkout VibroSim-$VERSION
+  git submodule update
+done
+
+cd /tmp
+zip -r VibroSim-$VERSION.zip VibroSim-$VERSION
+cp /tmp/VibroSim-$VERSION.zip /usr/local/src/
+
+echo "It might be nice to also generate the .mtlbx version of VibroSim_COMSOL and add that to the .ZIP file /usr/local/src/VibroSim-$VERSION"
